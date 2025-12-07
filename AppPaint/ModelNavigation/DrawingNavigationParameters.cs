30e331e10e473ceb08fa7b5e90cb6d4020944285
22120209 <minhpqdp02@gmail.com>
2025-12-07 11:13:16 +0700

    Add DrawingNavigationParameters class for canvas navigation

---
+++ AppPaint/ModelNavigation/DrawingNavigationParameters.cs
@@ -0,0 +1,23 @@
+namespace AppPaint.Models;
+
+/// <summary>
+/// Parameters for navigating to DrawingCanvasPage
+/// </summary>
+public class DrawingNavigationParameters
+{
+    /// <summary>
+    /// Profile ID to apply settings from
+    /// </summary>
+    public int ProfileId { get; set; }
+
+    /// <summary>
+    /// Drawing/Template ID to load (null = new blank drawing)
+    /// </summary>
+    public int? DrawingId { get; set; }
+
+    public DrawingNavigationParameters(int profileId, int? drawingId = null)
+    {
+        ProfileId = profileId;
+        DrawingId = drawingId;
+    }
+}
