2b48fe2ebd706b928aae2f0ed49f6da626b25fb8
22120209 <minhpqdp02@gmail.com>
2025-12-07 09:24:25 +0700

    Refactor DrawingNavigationParameters for consistent formatting and indentation

--- AppPaint/Models/DrawingNavigationParameters.cs
+++ AppPaint/Models/DrawingNavigationParameters.cs
@@ -9,15 +9,15 @@ public class DrawingNavigationParameters
     /// Profile ID to apply settings from
     /// </summary>
     public int ProfileId { get; set; }
-    
+
     /// <summary>
     /// Drawing/Template ID to load (null = new blank drawing)
     /// </summary>
     public int? DrawingId { get; set; }
-    
+
     public DrawingNavigationParameters(int profileId, int? drawingId = null)
     {
         ProfileId = profileId;
-  DrawingId = drawingId;
+        DrawingId = drawingId;
     }
 }
